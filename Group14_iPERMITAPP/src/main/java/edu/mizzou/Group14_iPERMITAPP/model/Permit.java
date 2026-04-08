4ab1337e4b5cd62e8f59328958cf5534309d0099
Zach Goodson <quantamstudios@gmail.com>
2026-04-07 19:42:08 -0500

    Add Fields to Permit

--- Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/Permit.java
+++ Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/Permit.java
@@ -1,5 +1,25 @@
 package edu.mizzou.Group14_iPERMITAPP.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Data
 public class Permit {
 
+    @Id
+    private String permitID;
+
+    private Date dateOfIssue;
+    private String duration;
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "permit_request_id")
+    private PermitRequest permitRequest;
+
+    @ManyToOne
+    @JoinColumn(name = "eo_id")
+    private EO eo;
 }
